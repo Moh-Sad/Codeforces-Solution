f9787f0866a7e09bf45c643cabfd715faf70d849
Mohammed Sadik <codeprogrammer111@gmail.com>
2024-08-25 00:24:27 +0300

    Create A - In Search of an Easy Problem.py

---
+++ A - In Search of an Easy Problem.py
@@ -0,0 +1,10 @@
+"""            Author: Mohammed Sadik
+               Language: Python 3
+               Difficulty: Easy              """
+
+n = int(input())
+items = input().split()
+if "1" in items:
+    print("HARD")
+else:
+    print("EASY")
