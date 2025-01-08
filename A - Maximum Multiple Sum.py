c67f1503bbd16c24b8f5f2cdfd546cf994ecbbf5
Mohammed Sadik <codeprogrammer111@gmail.com>
2025-01-08 21:09:12 +0300

    Create A - Maximum Multiple Sum.py

---
+++ A - Maximum Multiple Sum.py
@@ -0,0 +1,10 @@
+"""             Author: Mohammed Sadik
+                Language: Python 3
+                Difficulty: Medium               """
+
+for _ in range(int(input())):
+    num = int(input())
+    if num - 2 >= 2:
+        print(2)
+    else:
+        print(num)
