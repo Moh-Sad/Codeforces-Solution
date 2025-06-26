cd7fc21a3cd5657b6b83db5d9940049593daeda1
Mohammed Sadik <codeprogrammer111@gmail.com>
2025-06-26 21:59:53 +0300

    Add solution for A - Nebiyu and a Contest problem

---
+++ A - Nebiyu and a Contest.py
@@ -0,0 +1,11 @@
+"""    
+Author: Mohammed Sadik
+Language: Python 3
+Difficulty: Easy    
+"""
+
+n, m, k = map(int, input().split())
+if m >= n and k >= n:
+    print("Yes")
+else:
+    print("No") 
